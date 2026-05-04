90f3fd0b87cac757139a0836a9384ea12fcc19c6
calvintorsaGit <calvintorsa@gmail.com>
2026-05-04 12:22:26 +0300

    fix: unfix conflict code

--- billing-service/src/test/java/com/unicampus/billing/BillingServiceApplicationTests.java
+++ billing-service/src/test/java/com/unicampus/billing/BillingServiceApplicationTests.java
@@ -3,11 +3,7 @@ package com.unicampus.billing;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-<<<<<<< HEAD
 @SpringBootTest
-=======
-@SpringBootTest(properties = "debug=false")
->>>>>>> 90a899274f252ead0c8ed43ebd487103c28a3074
 class BillingServiceApplicationTests {
 
     @Test
