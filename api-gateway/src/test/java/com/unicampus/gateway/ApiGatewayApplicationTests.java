bd9c7a3a32dee24957bc30facda54cba602d8261
calvintorsaGit <calvintorsa@gmail.com>
2026-05-10 19:50:29 +0300

    feat: add apigateway

--- api-gateway/src/test/java/com/unicampus/gateway/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/com/unicampus/gateway/ApiGatewayApplicationTests.java
@@ -1,12 +1,13 @@
-package com.unicampus.dormitory;
+package com.unicampus.gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DormitoryServiceApplicationTests {
+class ApiGatewayApplicationTests {
 
     @Test
     void contextLoads() {
+        // Validates that the gateway application context starts successfully
     }
 }
